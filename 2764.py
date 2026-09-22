46019d601146a70dcca7cfd0f26f52918dfd3765
William Bruno <williambrunoclar@gmail.com>
2026-09-22 13:28:35 -0300

    2764

---
+++ 2764.py
@@ -0,0 +1,12 @@
+dia, mes, ano = map(str, input().split("/"))
+
+data1 = mes+"/"+dia+"/"+ano
+data2 = ano+"/"+mes+"/"+dia
+data3 = dia+"-"+mes+"-"+ano
+
+print(data1)
+print(data2)
+print(data3)
+
+
+
