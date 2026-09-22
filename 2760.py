010a3b1d4085d92ac8f5264be3c23287b3aff598
William Bruno <williambrunoclar@gmail.com>
2026-09-22 14:29:39 -0300

    2760

---
+++ 2760.py
@@ -0,0 +1,11 @@
+a,b,c = [input() for _ in range(3)]
+
+frase1 = a+b+c
+frase2 = b+c+a
+frase3 = c+a+b
+frase4 = a[0:10]+b[0:10]+c[0:10]
+
+print(frase1)
+print(frase2)
+print(frase3)
+print(frase4)
